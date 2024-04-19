46f8479085e6521dc8cbef68248b029e332998ef
lubnashaikh123 <128475990+lubnashaikh123@users.noreply.github.com>
2024-04-19 21:17:24 +0530

    Fair-Task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FairDashboardComponent } from './shared/components/fair-dashboard/fair-dashboard.component';
+import { FairCardComponent } from './shared/components/fair-dashboard/fair-card/fair-card.component';
+import { FairInfoComponent } from './shared/components/fair-dashboard/fair-info/fair-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FairDashboardComponent,
+    FairCardComponent,
+    FairInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
